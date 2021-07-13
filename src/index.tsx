f3e131f64cbf6d8d1047d415134c6706a08bdda9
YouRa <allakhverdovyuriy@gmail.com>
2021-07-13 19:46:53 +0300

    Непонятное состояние компонента

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ interface AppProps {
 
 class App extends React.Component<AppProps> {
 
-  state = {counter: 0};
+  state = { counter: 0 };
 
   onIncrement = (): void => {
     this.setState({ counter: this.state.counter + 1});
