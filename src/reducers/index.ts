d4d91083cf6b3fcd54546a094d65b61daeb85d95
YouRa <allakhverdovyuriy@gmail.com>
2021-07-14 13:28:45 +0300

    Валидация структуры Redux Store

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
 
-export const reducers = combineReducers({
-  counter: () => 0
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer
 });
